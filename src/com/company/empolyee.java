d1d802bb5df22ba99ba4b0c4f34b76f023c80c9d
Ahmed Tolpa <ahmedtolpamm@gmail.com>
2022-12-13 13:53:46 +0200

    tmam

--- src/com/company/empolyee.java
+++ src/com/company/empolyee.java
@@ -1,98 +1,54 @@
 package com.company;
 
-import java.io.*;
 import java.util.ArrayList;
-import java.util.Scanner;
 
-public class empolyee implements Serializable {
-    public Object array;
-    String name ;
-    static int count=1;
-    int id ;
-    double salary =id*1000 ;
+public class empolyee {
+    int id;
+    String name;
+    double salary;
 
-
-
-
-
-
-    public empolyee(String name){
-
-        this.name = name ;
-        this.id =count;
-        this.salary=count*1000;
-        count ++;
-
+    public String toString(){
+        return id+" "+ name+" " +salary ;
     }
 
 
-    public String getName() {
-        return name;
+    // depart d1;
+
+    depart d1=new depart(20,"tolpa","HTML");
+    depart d2 = new depart();
+
+
+
+
+    public empolyee(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
     }
 
-    public static int getCount() {
-        return count;
-    }
 
-    public int getId() {
+
+    public void setId(int i ){
+        id=i;
+    }
+    public void setName(String n){
+        name=n;
+    }
+    public void setSalary(double s){
+        salary=s;
+    }
+    public int getId(){
         return id;
     }
-
-    public double getSalary() {
+    public String getName(){
+        return name;
+    }
+    public double getSalary(){
         return salary;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public static void setCount(int count) {
-        empolyee.count = count;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
-    public String toString(){
-
-        String s ;
-        s ="name" +" "+ name +" id " +id + " " + salary;
-        return s;
-    }
-
-    public void  array() throws IOException, ClassNotFoundException {
-        ArrayList Emp=new ArrayList();
-        Scanner in =new Scanner(System.in);
-        System.out.println("enter empolyee name");
-        for (int e =1 ;e< 10; e++){
-            String s ;
-            s ="name" +" "+ name +" id " +id + " " + salary;
-            Emp.add(in.next());
-            toString();
-            System.out.println(Emp);
-
-        }
-        File file = new File("Emp");
-        PrintWriter outputfile =new PrintWriter("Emp");
-        outputfile.println(Emp);
-
-//        for (int i=0 ;i<Emp.size();i++){
-//            outputfile.println(Emp);
-//        }
-        outputfile.close();
 
 
-
-    }
 
 
 }
-
-
-
-
-
